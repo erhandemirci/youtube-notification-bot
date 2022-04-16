57db9975d2814b3d022baf9ddfa0bac15e270748
Erhan <demirci.erhan@windowslive.com>
2022-04-16 22:10:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "YOUR_DISCORD_BOT_TOKEN",
-    channel: "XXXXXXXXXXXXXXXXXX",
+    token: "OTY0OTY1NDQ5MzI0NDMzNDM4.YlsUGw.u68cyIkrrIdjXtAkz2flEujYNuw",
+    channel: "964964346717417573",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel_id: "UC1zAttFQKikWoKH3Vb39ETA",
     watchInterval: 30000
 }
